13e385750be07bb914749cd75612e3bba03d71f6
Dikesh Kumar <117905520+Dikesh01@users.noreply.github.com>
2023-06-21 01:03:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+
+const head1 = document.getElementById("heading1");
+const head2 = document.getElementById("heading2");
+const head3 = document.getElementById("heading3");
+
